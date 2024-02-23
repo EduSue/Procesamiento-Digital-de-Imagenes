d849f81bcda818e7bf48cba81e01989eb1cc5c78
EduSue <edu793.c.m@gmail.com>
2024-02-23 19:41:01 -0400

    Primera prueba con git, CRUD y fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,22 @@
+h = input()
+h = float(h)
+
+# print(h)
+if h == 0:
+    print(0)
+if h == 1:
+    print(1)
+else:
+    o = 0
+    n = 0
+    m = 1
+    r = 0
+    while o < h:
+        r = n
+        
+        n = n + m
+
+        m = r
+
+        o = o + 1
+        print(n)
